049095779079a140806447ef11e631801ec42ec6
irisdew <yclaredew@gmail.com>
2021-06-18 19:48:43 +0900

    tutorial 6 - Setup Redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./styles.scss";
 import { Link } from "react-router-dom";
 import { auth } from "./../../firebase/utils";
@@ -43,4 +44,11 @@ Header.defaultProps = {
   currentUser: null,
 };
 
-export default Header;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의
+// 현재 리덕스 상태를 파라미터로 받아옵니다.
+
+export default connect(mapStateToProps, null)(Header);
+// 받아오기만하고 dispatch는 안할 것이므로 null
